c1a26b1eb2736e063f4b804226fe09655d23d9de
James Dunwoody <jamesdunwoody@gmail.com>
2011-02-19 17:03:41 +1100

    Implemented first attempt at process and network usage class

--- src/com/james/AndroidDataOffenders.java
+++ src/com/james/AndroidDataOffenders.java
@@ -1,13 +1,30 @@
 package com.james;
 
 import android.app.Activity;
+import android.app.ActivityManager;
+import android.app.ActivityManager.RunningAppProcessInfo;
+import android.net.TrafficStats;
 import android.os.Bundle;
+import android.os.Process;
 
 public class AndroidDataOffenders extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		TrafficStats stats = new TrafficStats();
+
+		stats.getMobileRxBytes();
+		stats.getTotalRxBytes();
+
+		int myUid = Process.myUid();
+		ActivityManager activityManager = (ActivityManager) getSystemService(ACTIVITY_SERVICE);
+
+		for (RunningAppProcessInfo processInfo : activityManager.getRunningAppProcesses()) {
+			System.out.println(processInfo.processName + " hase used " + stats.getUidRxBytes(processInfo.uid) + " bytes");
+		}
+
+		setContentView(R.layout.main);
+	}
 }
