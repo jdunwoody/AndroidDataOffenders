4049c6dcda23e59b42ba84a797aa96c97fa042e5
James Dunwoody <jamesdunwoody@gmail.com>
2011-02-19 14:34:51 +1100

    Created Android base

---
+++ src/com/james/AndroidDataOffenders.java
@@ -0,0 +1,13 @@
+package com.james;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AndroidDataOffenders extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
